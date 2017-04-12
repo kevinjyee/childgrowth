9ab0efb482ce94f197b9f1414854c261698a2ee5
kevinjyee <kevjyee@gmail.com>
2017-04-12 11:04:13 -0500

    added buttons and image handlers

--- app/src/main/java/com/childgrowth/childgrowth/MainActivity.java
+++ app/src/main/java/com/childgrowth/childgrowth/MainActivity.java
@@ -1,13 +1,47 @@
 package com.childgrowth.childgrowth;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.view.LayoutInflater;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Fragment implements View.OnClickListener {
+    private Button profilebutton;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_main, null);
+        //Profile Button
+        profilebutton = (Button) view.findViewById(R.id.profilebutton);
+        profilebutton.setOnClickListener(this);
+        //Include More buttons below as we see fit
+
+        //Include
+
+
+        //Include
+        return view;
+
     }
-}
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.profilebutton) {
+            profilebutton.setEnabled(false);
+
+        }
+        }
+
+        }
+
+
---
+++ app/src/main/java/com/childgrowth/childgrowth/ProfileActivity.java
@@ -0,0 +1,11 @@
+package com.childgrowth.childgrowth;
+
+/**
+ * Created by Kevin on 4/12/2017.
+ */
+
+public class ProfileActivity {
+    public ProfileActivity() {
+        super();
+    }
+}
