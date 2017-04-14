871eec0b59f2cc5ebd23ea47662d8ded327f2122
kevinjyee <kevjyee@gmail.com>
2017-04-14 08:43:40 -0500

    added front title screen

--- app/src/main/java/com/childgrowth/childgrowth/MainActivity.java
+++ app/src/main/java/com/childgrowth/childgrowth/MainActivity.java
@@ -1,6 +1,8 @@
 package com.childgrowth.childgrowth;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,37 +11,54 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.view.LayoutInflater;
 
-public class MainActivity extends Fragment implements View.OnClickListener {
-    private Button profilebutton;
+import mehdi.sakout.fancybuttons.FancyButton;
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    private Button profileButton;
+    private Button infoButton;
+    private Button historybutton;
+    private Button settingsbutton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+
+        profileButton = (Button) findViewById(R.id.button1);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent profileIntent = new Intent(MainActivity.this,ProfileActivity.class);
+                MainActivity.this.startActivity(profileIntent);
+            }
+        });
+
+        infoButton = (Button) findViewById(R.id.button2);
+       infoButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        historybutton = (Button) findViewById(R.id.button3);
+        historybutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        settingsbutton = (Button) findViewById(R.id.button4);
+        settingsbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
     }
 
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_main, null);
-        //Profile Button
-        profilebutton = (Button) view.findViewById(R.id.profilebutton);
-        profilebutton.setOnClickListener(this);
-        //Include More buttons below as we see fit
-
-        //Include
-
-
-        //Include
-        return view;
-
-    }
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.profilebutton) {
-            profilebutton.setEnabled(false);
 
-        }
         }
 
         }
--- app/src/main/java/com/childgrowth/childgrowth/ProfileActivity.java
+++ app/src/main/java/com/childgrowth/childgrowth/ProfileActivity.java
@@ -1,11 +1,27 @@
 package com.childgrowth.childgrowth;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by Kevin on 4/12/2017.
  */
 
-public class ProfileActivity {
-    public ProfileActivity() {
-        super();
+
+public class ProfileActivity extends Activity implements View.OnClickListener {
+    private Button profilebutton;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
     }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
 }
