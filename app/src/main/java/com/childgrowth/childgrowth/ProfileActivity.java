aacb4340d328701c5971fc31a6706cb617f51fe8
kevinjyee <kevjyee@gmail.com>
2017-04-16 18:30:42 -0500

    added profiles page

--- app/src/main/java/com/childgrowth/childgrowth/ProfileActivity.java
+++ app/src/main/java/com/childgrowth/childgrowth/ProfileActivity.java
@@ -10,18 +10,51 @@ import android.widget.Button;
  */
 
 
-public class ProfileActivity extends Activity implements View.OnClickListener {
-    private Button profilebutton;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.childgrowth.childgrowth.utilities.CustomListAdapter;
+
+
+public class ProfileActivity extends Activity{
+
+    ListView list;
+    String[] itemname ={
+            "Kevin",
+            "Theresa"
+    };
+
+    Integer[] imgid={
+            R.drawable.baby_sample1,
+            R.drawable.baby_sample2,
+
+    };
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        CustomListAdapter adapter=new CustomListAdapter(this, itemname, imgid);
+        list=(ListView)findViewById(R.id.android_list);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String Slecteditem= itemname[+position];
+                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
-
-
-    @Override
-    public void onClick(View v) {
-
-    }
-
 }
