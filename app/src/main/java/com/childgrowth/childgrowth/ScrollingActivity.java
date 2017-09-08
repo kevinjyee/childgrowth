a30a5d546c34b37acaad9267fc27683659bed153
kevinjyee <kevjyee@gmail.com>
2017-09-08 11:54:52 -0500

    added line graphing

--- app/src/main/java/com/childgrowth/childgrowth/ScrollingActivity.java
+++ app/src/main/java/com/childgrowth/childgrowth/ScrollingActivity.java
@@ -27,17 +27,49 @@ public class ScrollingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mChart = (BarChart) findViewById(R.id.chart1);
+
+        mChart.getDescription().setEnabled(false);
+
+        // scaling can now only be done on x- and y-axis separately
+        mChart.setPinchZoom(false);
+
+        mChart.setDrawBarShadow(false);
+        mChart.setDrawGridBackground(false);
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setPosition(XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+
+        mChart.getAxisLeft().setDrawGridLines(false);
+
+        mChart.getLegend().setEnabled(false);
+
+        setData(10);
+        mChart.setFitBars(true);
+    }
+
+    private void setData(int count) {
+
+        ArrayList<BarEntry> yVals = new ArrayList<BarEntry>();
+
+        for (int i = 0; i < count; i++) {
+            float val = (float) (Math.random() * count) + 15;
+            yVals.add(new BarEntry(i, (int) val));
+        }
+
+        BarDataSet set = new BarDataSet(yVals, "Data Set");
+        set.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        set.setDrawValues(false);
+
+        BarData data = new BarData(set);
+
+        mChart.setData(data);
+        mChart.invalidate();
+        mChart.animateY(800);
     }
 }
